2f202a51a80cf912510e765428f29a3c964f14b3
Raimond Lige <raimlige@tlu.ee>
2026-02-10 11:02:37 +0000

    Homework vol 1

---
+++ 01_funktsioon/homework.js
@@ -0,0 +1,32 @@
+//Function - Volatile Spotify royality calculator
+//Calculates the pay per stream usually around 0.002-0.005 EUR (depends on the country and plan mostly)
+function royalityCalc(streams) {
+    var minRate = 0.002;
+    var maxRate = 0.005;
+    var randomRate = Math.random() * (maxRate - minRate) + minRate;
+    var income = streams * randomRate;
+    return Math.round(income * 100) / 100;
+}
+;
+//Function - 1 Single song test
+var my_hit_song = royalityCalc(100000);
+console.log("100000 streams earns (roughly): " + my_hit_song + " EUR.");
+//Function - 2 Using array as streams input (Different songs on an album)
+var album_streams = [1500, 24000, 1000000, 5000, 37000];
+console.log("--- Streams per different songs ---");
+for (var _i = 0, album_streams_1 = album_streams; _i < album_streams_1.length; _i++) {
+    var stream = album_streams_1[_i];
+    console.log(royalityCalc(stream) + " EUR");
+}
+;
+//Function - 3 Income projection table
+var income_table = [];
+for (var s = 0; s <= 50000; s += 5000) {
+    income_table.push([
+        s,
+        royalityCalc(s)
+    ]);
+}
+;
+console.log("Streaming income projection:");
+console.log(income_table);
---
+++ 01_funktsioon/homework.ts
@@ -0,0 +1,32 @@
+//Function - Volatile Spotify royality calculator
+//Calculates the pay per stream usually around 0.002-0.005 EUR (depends on the country and plan mostly)
+function royalityCalc(streams: number): number {
+    const minRate = 0.002;
+    const maxRate = 0.005;
+    let randomRate = Math.random() * (maxRate - minRate) + minRate;
+    let income = streams * randomRate;
+    return Math.round(income * 100) / 100
+};
+
+//Function - 1 Single song test
+let my_hit_song = royalityCalc(100000);
+console.log("100000 streams earns (roughly): " + my_hit_song + " EUR.");
+
+//Function - 2 Using array as streams input (Different songs on an album)
+let album_streams: number[] = [1500, 24000, 1000000, 5000, 37000];
+console.log("--- Streams per different songs ---")
+for (let stream of album_streams) {
+    console.log(royalityCalc(stream) + " EUR")
+};
+
+//Function - 3 Income projection table
+let income_table: number[][] = [];
+for (let s = 0; s <= 50000; s += 5000) {
+    income_table.push([
+        s,
+        royalityCalc(s)
+    ]);
+};
+
+console.log("Streaming income projection:")
+console.log(income_table)

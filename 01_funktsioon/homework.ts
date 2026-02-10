d3c937fe5bad8f88668573065ce072e2e3dff37f
Raimond Lige <raimlige@tlu.ee>
2026-02-10 13:32:40 +0000

    Homework vol 2 - 10.02.26

--- 01_funktsioon/homework.ts
+++ 01_funktsioon/homework.ts
@@ -8,18 +8,18 @@ function royalityCalc(streams: number): number {
     return Math.round(income * 100) / 100
 };
 
-//Function - 1 Single song test
+//Adaptation - 1 Single song test
 let my_hit_song = royalityCalc(100000);
 console.log("100000 streams earns (roughly): " + my_hit_song + " EUR.");
 
-//Function - 2 Using array as streams input (Different songs on an album)
+//Adaptation - 2 Using array as streams input (Different songs on an album)
 let album_streams: number[] = [1500, 24000, 1000000, 5000, 37000];
 console.log("--- Streams per different songs ---")
 for (let stream of album_streams) {
     console.log(royalityCalc(stream) + " EUR")
 };
 
-//Function - 3 Income projection table
+//Adaptation - 3 Income projection table
 let income_table: number[][] = [];
 for (let s = 0; s <= 50000; s += 5000) {
     income_table.push([
